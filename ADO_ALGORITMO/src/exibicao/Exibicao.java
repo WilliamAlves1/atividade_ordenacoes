3ea69179bf94b8a3a912bc9c116ab5df5cbff39d
WilliamAlves1 <williamalvesg02@gmail.com>
2025-11-02 10:32:23 -0300

    Limitei o tamanho da janela de exibição dos vetores e adicionei uma barra de rolagem pros vetores médios e grandes.

--- ADO_ALGORITMO/src/exibicao/Exibicao.java
+++ ADO_ALGORITMO/src/exibicao/Exibicao.java
@@ -1,14 +1,37 @@
 package exibicao;
 
+import java.awt.Dimension;
 import java.util.Arrays;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class Exibicao {
-    public static void exibir(int[] vet){
+
+    public static void exibir(int[] vet) {
+        if (vet == null) {
+            JOptionPane.showMessageDialog(null,
+                    "Vetor está nulo. Não é possível exibir.",
+                    "Erro de Exibição",
+                    JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+
         String vetorString = Arrays.toString(vet);
 
-        JOptionPane.showMessageDialog(null, vetorString,
-                "Exibi��o do Vetor",
-                JOptionPane.INFORMATION_MESSAGE);
+        JTextArea textArea = new JTextArea(20, 50);
+        textArea.setText(vetorString);
+
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setEditable(false);
+        textArea.setCaretPosition(0);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        JOptionPane.showMessageDialog(null,
+                scrollPane,
+                "Exibição do Vetor (Tamanho: " + vet.length + ")",
+                JOptionPane.PLAIN_MESSAGE);
     }
 }
